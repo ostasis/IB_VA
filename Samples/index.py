489581621add9f7a176f5d9ef1b75dc516db8d77
ostasis <lachlan@ostasis.com.au>
2022-08-21 15:03:19 +0930

    update

--- Samples/index.py
+++ Samples/index.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+flask_app = Flask(__name__)
 
 
-@app.route("/")
-@app.route("/form")
+@flask_app.route("/")
+@flask_app.route("/form")
 def form():
     return render_template("form.html")
 
 
-@app.route("/data", methods=["POST", "GET"])
+class Test:
+    def __init__(self) -> None:
+        self.amount = 1000
+
+
+test = Test()
+
+
+@flask_app.route("/data", methods=["POST", "GET"])
 def data():
     if request.method == "GET":
         return (
@@ -21,9 +29,13 @@ def data():
         for x in form_data.to_dict():
             print(x)
             print(form_data.to_dict()[x])
+            if x == "Amount":
+                test.amount = form_data.to_dict()[x]
+
+        print(test.amount)
 
         return render_template("data.html", form_data=form_data)
 
 
 if __name__ == ("__main__"):
-    app.run(debug=True)
+    flask_app.run(debug=True)
